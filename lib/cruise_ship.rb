bf1cc899c681326e690bdafe67e01c63a4686170
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-03 16:49:56 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  passengers.each do |key,value|
+  passengers.each do | key, value|
     if ( key == :suite_a) && (name.start_with?('A'))
       winner = value
     end
